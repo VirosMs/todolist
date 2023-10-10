672d18c85d55474b0d6b05ccbf518844eacff361
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-10 11:23:29 +0200

    Primeira Clase de springBoot

---
+++ src/test/java/com/charles/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
