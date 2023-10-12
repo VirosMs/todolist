ae7a248ff4cf65bfcc68d274f3b245c725c78cf2
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-12 13:22:00 +0200

    Segunda Aula

--- src/main/java/com/charles/todolist/user/UserController.java
+++ src/main/java/com/charles/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package com.charles.todolist.user;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
-        System.out.println(userModel.getName());
-        System.out.println(userModel.getPassword());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            //Mensagem de erro
+            //Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/com/charles/todolist/user/UserModel.java
+++ src/main/java/com/charles/todolist/user/UserModel.java
@@ -1,33 +1,28 @@
 package com.charles.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
 
-    private String username;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
+    @Column(unique = true)
+    private String username;
     private String name;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
